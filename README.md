4d40cbf9c70768a91e43106e50968f48c73717e1
Frank Chang <frank@csie.io>
2020-08-11 14:39:06 +0800

    Added setup.py and update readme file.

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Currently only SOCKS5 without authentication and CONNECT command are supported.
 Python >= 3.7
 
 ## Quick guide
+
+### Install from PyPI
+```bash
+python3 -m pip install simple-socks5
+python -m socks5.server -H 127.0.0.1 -P 8888
+```
+
+### Get latest version from GitHub
 ```bash
 git clone https://github.com/frankurcrazy/socks5
 cd socks5 && python -m socks5.server -H 127.0.0.1 -P 8888
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-socks5",
+    version="0.0.1",
+    author="Frank Chang",
+    author_email="frank@csie.io",
+    description="A simple SOCKS5 server implementation with AsyncIO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frankurcrazy/socks5",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: System :: Networking",
+    ],
+    keywords=["socks", "socks5", "asyncio"],
+    python_requires='>=3.7',
+)
